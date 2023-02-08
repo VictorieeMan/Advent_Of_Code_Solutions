63e9b09fc23a18c9dae9a682f31581321969cfbc
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-02-08 12:05:47 +0100

    Prepping tools for solutions
    Added two files. Planning on making a data extractor that stores the test data from the site for easy use by every solution file.

---
+++ aoc_test-data-extractor.py
@@ -0,0 +1,12 @@
+"""Created: 2023-02-08
+Purpose: Extract the input data from the https://adventofcode.com website,
+and store it in an easy machine readable file structure for the individual
+solutions to read from. Used for testing code.
+
+The online data have the following link structure:
+https://adventofcode.com/20YY/day/DD/input
+
+YY >= 15, in [15,<This Year>]
+DD in [1,25]
+"""
+
