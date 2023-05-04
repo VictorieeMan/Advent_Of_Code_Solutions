44889c64d5a2eda1b481eafe08fe3f7187c153fc
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-05-04 17:30:35 +0200

    solved part 1

--- events/2015/day08/2015-08-sol.py
+++ events/2015/day08/2015-08-sol.py
@@ -1,4 +1,4 @@
-"""Created: 2023-, by @VictorieeMan
+"""Created: 2023-05-04, by @VictorieeMan
 Repository url: https://github.com/VictorieeMan/Advent_Of_Code_Solutions
 Event url: https://adventofcode.com/2015/day/8
 """
@@ -13,8 +13,44 @@ import aoc2015_tools as at
 ### START SOLUTION BODY ###
 ### Part 1 ###
 
+def count_str_chars(string):
+    count = -2 #Excluding the beginning and ending " marks
+    length = len(string)
+    count += length
+
+    #Search for all backslashes
+    backslash_pos = [i for i, c in enumerate(string) if c == '\\']
+
+    if len(backslash_pos) > 0:
+        i = 0
+        while i <= max(backslash_pos):
+            if i in backslash_pos:
+                #Due to input data, i+1 will never be out of bounce.
+                if string[i+1] == '\\' or string[i+1] == '\"':
+                    #Either a backslash or double-quote plus it's escapesequence
+                    #We subtract the double count of the escape sequence char.
+                    count -= 1
+                    i += 1
+                elif string[i+1] == 'x':
+                    #Signals a hexadecimal noted char
+                    #ex. \x27 is four chars, but represents one char
+                    #we correct for the +3 overcount in the totals.
+                    count -= 3
+            i += 1
+
+    return count
+
 def partOne(input):
-    pass
+    total_code_chars = 0
+    total_str__chars = 0
+    for line in input:
+        total_code_chars += len(line)
+        total_str__chars += count_str_chars(line)
+        # print(len(line),count_str_chars(line))
+    
+    ans = total_code_chars - total_str__chars
+    print(ans)
+
 	
 ### Part 2 ###
 def partTwo(input):
@@ -22,5 +58,6 @@ def partTwo(input):
 
 ### Main ###
 input = at.input_to_string(__file__,"input.txt")
+input = input.split('\n')[:-1] #Disregard last row, that is empty.
 partOne(input)
 partTwo(input)
