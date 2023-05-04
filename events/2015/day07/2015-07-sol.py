056bf43e5b9914da8bf8811557e26236f277232e
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-05-04 16:13:45 +0200

    part one solved

--- events/2015/day07/2015-07-sol.py
+++ events/2015/day07/2015-07-sol.py
@@ -1,4 +1,4 @@
-"""Created: 2023-, by @VictorieeMan
+"""Created: 2023-05-04, by @VictorieeMan
 Repository url: https://github.com/VictorieeMan/Advent_Of_Code_Solutions
 Event url: https://adventofcode.com/2015/day/7
 """
@@ -13,8 +13,108 @@ import aoc2015_tools as at
 ### START SOLUTION BODY ###
 ### Part 1 ###
 
+def convert_if_int(string):
+    try:
+        return int(string)
+    except:
+        return string
+
+def parsing_couplings(coupling, circuit_board):
+    """Storing couplings in dictionary.
+    Using coupling output as keys,
+    and as dictionary values either their direct integer signal
+    or coupling instructions as an array.
+
+    key: 'Value/Instruction'
+    """
+    array = coupling.split()
+    array_length = len(array)
+    if array_length == 3:
+        circuit_board[array[-1]] = convert_if_int(array[0])
+    else:
+        array[0] = convert_if_int(array[0])
+        array[2] = convert_if_int(array[2])
+        circuit_board[array[-1]] = array[0:-2]
+
+#Bitwise operations
+def AND(a,b):
+    return a&b
+
+def OR(a,b):
+    return a|b
+
+def LSHIFT(a,b):
+    return a<<b
+
+def RSHIFT(a,b):
+    return a>>b
+
+def NOT(a):
+    return 65535-a
+
+def check_int(test):
+    return isinstance(test,int)
+
+def check_str(test):
+    return isinstance(test,str)
+
+#Recursive calculation of circuit board
+def fetch_and_operate(target,board):
+    """On successful evaluation the board is also dynamically update,
+    to save computing power and not having to redo recursive tracks.
+    """
+    # print(target) #Good print for debug
+    if check_int(target):
+        return target
+    elif check_int(board[target]):
+        return board[target]
+    elif check_str(board[target]):
+        return fetch_and_operate(board[target],board)
+    else:
+        if board[target][0] == 'NOT':
+            a = fetch_and_operate(board[target][1],board)
+            ans = NOT(a)
+            board[target] = ans
+            return ans
+        
+        else:
+            a = fetch_and_operate(board[target][0],board)
+            if board[target][1] == 'AND':
+                b = fetch_and_operate(board[target][2],board)
+                ans = AND(a,b)
+                board[target] = ans
+                return ans
+            
+            elif board[target][1] == 'OR':
+                b = fetch_and_operate(board[target][2],board)
+                ans = OR(a,b)
+                board[target] = ans
+                return ans
+            
+            elif board[target][1] == 'LSHIFT':
+                b = board[target][2]
+                ans = LSHIFT(a,b)
+                board[target] = ans
+                return ans
+            
+            elif board[target][1] == 'RSHIFT':
+                b = board[target][2]
+                ans = RSHIFT(a,b)
+                board[target] = ans
+                return ans
+
 def partOne(input):
-    pass
+    board = {} #Build circuit board, store as dictionary.
+    for coupling in input:
+        if coupling == '':
+            continue
+        parsing_couplings(coupling, board)
+    
+    for target in board:
+        signal = fetch_and_operate(target,board)
+        print(target,': ',signal)
+    
+    print(fetch_and_operate('a',board))
 	
 ### Part 2 ###
 def partTwo(input):
@@ -22,5 +122,6 @@ def partTwo(input):
 
 ### Main ###
 input = at.input_to_string(__file__,"input.txt")
+input = input.split('\n')
 partOne(input)
 partTwo(input)
