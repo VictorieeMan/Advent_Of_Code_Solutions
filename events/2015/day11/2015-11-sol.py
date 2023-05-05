eed2b9d09e00ee03ac8807fd77789acc7307df85
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-05-05 14:23:39 +0200

    part one, solved!

--- events/2015/day11/2015-11-sol.py
+++ events/2015/day11/2015-11-sol.py
@@ -13,7 +13,76 @@ import aoc2015_tools as at
 ### START SOLUTION BODY ###
 ### Part 1 ###
 
+def increment_password(password):
+    """Increments password one setp.
+    """
+    for i in range(len(password)-1,-1,-1):
+        if password[i] == 25:
+            password[i] = 0
+        else:
+            password[i] += 1
+            break
+    return password
+
+def check_and_fix_forbidden_chars(password,forbidden):
+    """Check if password contains forbidden chars and fix if any.
+    """
+    for i in range(len(password)):
+        if password[i] in forbidden:
+            password[i] += 1 #Increment to next letter
+            for j in range(i+1,len(password)):
+                #Set all following letters to 0:'a'
+                password[j] = 0
+
+def check_for_triple_consecutive_letters(password):
+    """Check if password contains triple consecutive letters,
+    ex. 'abc' or 'xyz'. At least one tirple is required.
+    """
+    for i in range(len(password)-2):
+        if password[i] == password[i+1]-1 and password[i] == password[i+2]-2:
+            return True
+    return False
+
+def check_for_two_seperate_letter_pairs(password):
+    """Looking for two non overlapping pairs of letters in the string,
+    ex. aa and aa or xx and yy
+    """
+    count = 0
+    skip = None
+    for i in range(len(password)-1):
+        if i != skip and password[i] == password[i+1]:
+            count += 1
+            skip = i + 1
+            if count == 2:
+                return True
+            
+    return False
+
+
 def partOne(input):
+    alphabet = dict(zip(range(26),"abcdefghijklmnopqrstuvwxyz"))
+    alphanum = dict(zip("abcdefghijklmnopqrstuvwxyz",range(26)))
+    #Converting input to a list of numbers
+    password = [alphanum[letter] for letter in input]
+
+    #Storing forbidden chars
+    forbidden = [alphanum[letter] for letter in "iol"]
+
+    #Find the next valid password
+    while True:
+        increment_password(password)
+        #Check if password is valid
+        check_and_fix_forbidden_chars(password,forbidden)
+        if check_for_triple_consecutive_letters(password):
+            if check_for_two_seperate_letter_pairs(password):
+                break
+         
+    print(password)
+
+    #Converting password numbers to a list of chars
+    password = [alphabet[numb] for numb in password]
+    print(''.join(password))
+
     print("Part 1, Done!\n")
 	
 ### Part 2 ###
@@ -22,6 +91,6 @@ def partTwo(input):
 
 ### Main ###
 input = at.input_to_string(__file__,"input.txt")
-input = input.split('\n')[:-1] #Skipping last line, that's usually empty.
+input = list(input[:-1]) #Input to char list.
 partOne(input)
 partTwo(input)
