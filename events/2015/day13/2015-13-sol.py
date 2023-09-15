5876a14d6ae1447f44ef994936f15d9d46b793d0
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-09-15 11:31:10 +0200

    Added a parsing functiuon for the input

--- events/2015/day13/2015-13-sol.py
+++ events/2015/day13/2015-13-sol.py
@@ -1,4 +1,4 @@
-"""Created: 2023-05-05, by @VictorieeMan
+"""Created: 2023-08-07, by @VictorieeMan
 Repository url: https://github.com/VictorieeMan/Advent_Of_Code_Solutions
 Event url: https://adventofcode.com/2015/day/13
 """
@@ -13,7 +13,23 @@ import aoc2015_tools as at
 ### START SOLUTION BODY ###
 ### Part 1 ###
 
+def parsing_input(input):
+    """Input: list of strings"""
+    """Sample:
+    "Alice would gain 2 happiness units by sitting next to Bob."
+    "Alice would lose 82 happiness units by sitting next to David."
+    After split() to a list, interesting data is indexed at [0,2,3,10]
+    Note: that last element at index 10 also contains the name and a "."
+    """
+    data = []
+    for str in input:
+        str = str.split()
+        data.append([str[0],str[2],str[3],str[10][:-1]]) # ['Alice', 'gain', '2', 'Bob']
+    return data
+
 def partOne(input):
+    data = parsing_input(input)
+    print(data)
     print("Part 1, Done!\n")
 	
 ### Part 2 ###
@@ -24,4 +40,4 @@ def partTwo(input):
 input = at.input_to_string(__file__,"input.txt")
 input = input.split('\n')[:-1] #Skipping last line, that's usually empty.
 partOne(input)
-partTwo(input)
+# partTwo(input)
